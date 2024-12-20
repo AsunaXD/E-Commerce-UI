c85144e946aa3cd701d71f9a5a16c64ebd30ddae
Asuna XD <ok4625685@gmail.com>
2024-12-20 18:30:13 +0530

    onboarding in process

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:e_commerce_ui/SCreens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "E-Commerce",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFFEF6969),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
