610af36d40413fd0f5c149f383c7838e4c9d4753
Asuna XD <ok4625685@gmail.com>
2025-01-03 20:03:30 +0530

    product pop up details

--- lib/SCreens/prdouct_screen.dart
+++ lib/SCreens/prdouct_screen.dart
@@ -1,5 +1,8 @@
 import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+import '../widgets/product_details_popup.dart';
 
 class PrdouctScreen extends StatelessWidget {
   List<String> images = [
@@ -33,6 +36,7 @@ class PrdouctScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         SizedBox(
                           height: 30,
@@ -48,9 +52,81 @@ class PrdouctScreen extends StatelessWidget {
                         SizedBox(
                           height: 5,
                         ),
-                        Text("Hoodie Jacket"),
+                        Text(
+                          "Hoodie Jacket",
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ],
-                    )
+                    ),
+                    Text(
+                      "\$300.00",
+                      style: TextStyle(
+                        color: Color(0xFFEF6969),
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RatingBar.builder(
+                    initialRating: 3,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemSize: 25,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: (rating) {
+                      print(rating);
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Cool, windy weather is on its way.Send him out\nthe door in the jacket he wants to wear. Warm \nZooper Hoodie Jacket",
+                    style: TextStyle(
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Color(0x1F989797),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.shopping_cart,
+                          color: Color(0xFFEF6969),
+                        ),
+                      ),
+                    ),
+                    ProductDetailsPopup(),
                   ],
                 ),
               ],
